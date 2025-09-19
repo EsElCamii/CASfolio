68664ba54ace10387eea52fe67e32ce4780644b3
EsElCamii <your.email@example.com>
2025-09-19 09:15:01 -0600

    chore: add @vercel/speed-insights and wire up <SpeedInsights /> in pages/_app.js

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
+}
+
